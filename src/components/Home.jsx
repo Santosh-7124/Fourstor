b0df8397e9d6e940b41a55ac6d0d819a34c2bf2f
Santosh-7124 <sunny962002@gmail.com>
2024-12-04 18:12:39 +0530

    Below Home Banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "./Home/Banner";
 import Text from "./Home/Text";
+import Advantage from "./Home/Advantage";
 
 function Home() {
   return (
     <>
       <Banner />
+      <Advantage/>
       <Text />
     </>
   );
