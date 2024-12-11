75a4c9b2b089949b610f75704ac0980432ed1c17
Santosh-7124 <sunny962002@gmail.com>
2024-12-11 11:17:36 +0530

    Home page responsive done

--- src/components/Home/Text.jsx
+++ src/components/Home/Text.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Image from "../../assets/Home/Precision Innovation Progress.png";
+import MobileImage from "../../assets/Home/Precision Innovation Progress Mobile.png";
 
 function Text() {
   return (
     <div className="home-text">
-      <img src={Image} alt="Fourstor Precision Innovation Progres" />
+      <img
+        src={Image}
+        className="desktop"
+        alt="Fourstor Precision Innovation Progres"
+      />
+      <img
+        src={MobileImage}
+        className="mobile"
+        alt="Fourstor Precision Innovation Progres"
+      />
     </div>
   );
 }
